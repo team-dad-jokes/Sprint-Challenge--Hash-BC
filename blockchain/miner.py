a2387024c5e65cb35ddfdc6ad6160fc7335e0e95
johnpitts <jgpitts@gmail.com>
2020-03-14 10:36:17 -0400

    ran miner.py on a totally new cloned instance on another part of my macbook, but requests simply won't work.  it's mind-boggling

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -23,8 +23,10 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+    proof = 3
+    last_hash = hashlib.sha256(str(last_proof).encode()).hexdigest()
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -39,8 +41,9 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
 
-    # TODO: Your code here!
-    pass
+    guess_hash = haslib.sha256(str(proof).encode()).hexdigest()
+    
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
@@ -63,6 +66,7 @@ if __name__ == '__main__':
         exit()
     # Run forever until interrupted
     while True:
+        
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
         data = r.json()
